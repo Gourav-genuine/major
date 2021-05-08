6edfcacdef7409d8ab8af6d944c2934fa1347061
bhavesh-chandrakar <bhavesh1334@gmail.com>
2021-05-08 20:28:57 +0530

    make changes on cameraInput

--- src/components/PutCameraInput.js
+++ src/components/PutCameraInput.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, ImageBackground} from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
+const options = {
+  title: 'Pick Image',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
 
 const PutCameraInput = () => {
-  const showOutput = () => {
+  const [image, setImage] = useState(
+    'https://d1nz104zbf64va.cloudfront.net/dt/a/o/a-few-words-about-artificial-intelligence-what-is-it.jpg',
+  );
+
+  const uploadImage = () => {
     console.warn('CameraInput sent');
+  };
+
+  const chooseImage = async () => {
+    launchImageLibrary(options, response => {
+      console.log('Response from launchCamera', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.errorMessage) {
+        console.log('ImagePicker: ', response.errorMessage);
+      } else {
+        console.log(response);
+        setImage(response.uri);
+      }
+    });
   };
 
   return (
@@ -20,24 +48,34 @@ const PutCameraInput = () => {
         style={{
           display: 'flex',
           justifyContent: 'center',
-          height: 300,
-          width: 300,
-          borderRadius: 30,
-          backgroundColor: 'purple',
           marginBottom: 30,
         }}>
-        <Text
+        <ImageBackground
+          resizeMode="contain"
           style={{
-            color: 'white',
-            textAlign: 'center',
-            fontSize: 30,
-            fontFamily: 'Poppins-Bold',
+            height: 300,
+            width: 300,
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+          source={{
+            uri: image,
           }}>
-          Choose Photo
-        </Text>
+          <TouchableOpacity onPress={chooseImage}>
+            <Text
+              style={{
+                color: 'white',
+                textAlign: 'center',
+                fontFamily: 'Poppins-Bold',
+                fontSize: 28,
+              }}>
+              Pick Image
+            </Text>
+          </TouchableOpacity>
+        </ImageBackground>
       </View>
 
-      <TouchableOpacity onPress={showOutput}>
+      <TouchableOpacity onPress={uploadImage}>
         <Text
           style={{
             color: 'white',
